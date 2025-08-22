6c5b71498437f5e97c022cf29d7de47b42d55372
Laksh Gandhi <lakshgandhi22@gmail.com>
2025-08-22 10:53:20 +0530

    Add files via upload

---
+++ lab2_4.c
@@ -0,0 +1,20 @@
+//wap to check the number whether it is divisible by 7 or not
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter any number[a] \n");
+scanf("%d" ,&a);
+
+if(a%7==0)
+
+        printf("the entered is divisible by 7 \n");
+
+ else
+
+
+      printf("the entered is not divisible by 7 \n");
+
+
+ return 0;
+}
