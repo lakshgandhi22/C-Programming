e50f0fc2966394d5991e1b5487409119ae593eba
Laksh Gandhi <lakshgandhi22@gmail.com>
2025-09-19 09:54:25 +0530

    Add files via upload

---
+++ lab3_11.c
@@ -0,0 +1,10 @@
+//wap to print my name 5 times
+#include<stdio.h>
+main()
+{
+    for(int i=1;i<6;i++)
+    {
+        printf("Laksh Gandhi \n");
+    }
+ return 0;
+}
---
+++ lab3_12.c
@@ -0,0 +1,11 @@
+//wap to print my name n times
+#include<stdio.h>
+main()
+{
+    int i,n;
+    for(i=1;i<=n;i++)
+    {
+        printf("Laksh Gandhi \n");
+    }
+ return 0;
+}
